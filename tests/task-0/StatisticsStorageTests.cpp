c0804f3932722b9fbefc9536c903c78b022c6e31
tangakk <glukhovoleg2007@gmail.com>
2026-09-23 09:09:40 +0700

    just one more test

--- tests/task-0/StatisticsStorageTests.cpp
+++ tests/task-0/StatisticsStorageTests.cpp
@@ -23,3 +23,32 @@ TEST(StatisticsStorageTests, test1) {
         ++it2;
     }
 }
+
+TEST(StatisticsStorageTests, test2) {
+    StatisticsStorage storage;
+    for (int i = 0; i < 100; ++i) {
+        storage.addWord("a");
+    }
+    for (int i = 0; i < 500; ++i) {
+        storage.addWord("b");
+    }
+    for (int i = 0; i < 200; ++i) {
+        storage.addWord("a");
+    }
+    storage.calculatePercentages();
+    list<pair<string, pair<int, float> > > expected = {
+            {"b", {500, 62.5f}},
+            {"a", {300, 37.5f}}
+    };
+    list<pair<string, pair<int, float> > > result = storage.getSortedStatistics();
+    ASSERT_EQ(result.size(), expected.size());
+    auto it1 = result.begin();
+    auto it2 = expected.begin();
+    while (it1 != result.end() && it2 != expected.end()) {
+        ASSERT_EQ(it1->first, it2->first);
+        ASSERT_EQ(it1->second.first, it2->second.first);
+        ASSERT_NEAR(it1->second.second, it2->second.second, 0.0001);
+        ++it1;
+        ++it2;
+    }
+}
